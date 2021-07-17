1bdc4883d69dc86c594dc9873e7dd3a5c8910bd6
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-07-17 14:29:10 +0530

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -2,7 +2,7 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 print("")
-print("""Welcome To W2HBot String Session\nGenerator By @W2HOwner\n\n""")
+print("""Welcome To KAALBot String Session\nGenerator By @KAALOwner\n\n""")
 print("""Enter Your Valid Details To Continue!\n\n """)
 
 API_KEY = input("API_ID:  ")
@@ -18,7 +18,7 @@ while True:
             session = client.session.save()
             client.send_message(
                 "me",
-                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it) \n\n `{session}` \n\n And Visit @W2HSupport For Any Help!\n\n",
+                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it) \n\n `{session}` \n\n And Visit @KAALSupport For Any Help!\n\n",
             )
 
             print(
